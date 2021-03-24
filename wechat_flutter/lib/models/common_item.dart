4ab530ead433eec3fa5b03b3e4f18218c8d797be
MacOMNI <414294494@qq.com>
2021-03-24 20:12:03 +0800

    #update me asset

--- wechat_flutter/lib/models/common_item.dart
+++ wechat_flutter/lib/models/common_item.dart
@@ -1,18 +1,24 @@
 class CommonItemModel {
   String image;
   String title;
+  String type;
   String haveLine;
+  String name;
   String sep;
   CommonItemModel({
     this.image,
     this.title,
     this.haveLine,
     this.sep,
+    this.type,
+    this.name,
   });
   CommonItemModel.fromJson(Map<String, dynamic> json) {
     image = json["avatar"];
     title = json["title"];
     haveLine = json["havline"];
     sep = json["sep"];
+    type = json["type"];
+    name = json["name"];
   }
 }
--- wechat_flutter/lib/views/pages/mine_page/mine_page.dart
+++ wechat_flutter/lib/views/pages/mine_page/mine_page.dart
@@ -1,6 +1,55 @@
 import 'dart:ui';
-
+import 'package:wechat_flutter/models/common_item.dart';
+import 'package:wechat_flutter/views/listItem/common_item_widget.dart';
 import 'package:flutter/material.dart';
+
+const _meList = [
+  {"avatar": "", "title": "", "type": "line", "sep": "1"},
+  {
+    "avatar": "assets/images/me/me_profile.png",
+    "title": "MacKun",
+    "name": "微信号:MacKunsj",
+    "type": "header",
+    "havline": "0",
+    "sep": "0"
+  },
+  {"avatar": "", "title": "", "havline": "0", "sep": "1"},
+  {
+    "avatar": "assets/images/me/me_album.png",
+    "title": "相册",
+    "type": "normal",
+    "havline": "0",
+    "sep": "0"
+  },
+  {
+    "avatar": "assets/images/me/me_save.png",
+    "title": "收藏",
+    "type": "normal",
+    "havline": "0",
+    "sep": "0"
+  },
+  {
+    "avatar": "assets/images/me/me_wallet.png",
+    "title": "钱包",
+    "havline": "0",
+    "type": "normal",
+    "sep": "0"
+  },
+  {"avatar": "", "title": "", "havline": "0", "sep": "1"},
+  {
+    "avatar": "assets/images/me/me_stickers.png",
+    "title": "表情",
+    "havline": "0",
+    "sep": "0"
+  },
+  {"avatar": "", "title": "", "havline": "0", "sep": "1"},
+  {
+    "avatar": "assets/images/me/me_settings.png",
+    "title": "设置",
+    "havline": "0",
+    "sep": "0"
+  },
+];
 
 class MinePage extends StatelessWidget {
   const MinePage({Key key}) : super(key: key);
@@ -10,17 +59,32 @@ class MinePage extends StatelessWidget {
     return Scaffold(
       appBar: new AppBar(
           brightness: Brightness.dark,
-          title: new Text("我", style: new TextStyle(fontSize: 22)),
+          title: new Text(
+            "发现",
+            style: new TextStyle(fontSize: 22),
+          ),
           flexibleSpace: ClipRect(
             child: BackdropFilter(
                 filter: ImageFilter.blur(sigmaX: 0, sigmaY: 2),
                 child: Container(color: Color(0xff2D2C33))),
           )),
-      body: new Center(
-        child: new Text(
-          "Me",
-          style: new TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-        ),
+      backgroundColor: Color(0xffF0F0F6),
+      body: ListView.builder(
+        itemCount: _meList.length,
+        itemBuilder: (BuildContext context, int index) {
+          CommonItemModel model = CommonItemModel.fromJson(_meList[index]);
+          if (model.sep == "1") {
+            return SizedBox(
+              height: 20,
+              width: double.infinity,
+            );
+          }
+          return CommonItemWidget(
+            image: model.image,
+            title: model.title,
+            height: 44,
+          );
+        },
       ),
     );
   }
