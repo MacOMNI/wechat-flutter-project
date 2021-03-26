fab108ef94d7faa4abc479ec0d45a432fa660d61
MacOMNI <414294494@qq.com>
2021-03-26 16:14:28 +0800

    #update user page

--- wechat_flutter/lib/views/pages/mine_page/user_page.dart
+++ wechat_flutter/lib/views/pages/mine_page/user_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class UserPage extends StatefulWidget {
@@ -10,6 +12,40 @@ class UserPage extends StatefulWidget {
 class _UserPageState extends State<UserPage> {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        brightness: Brightness.dark,
+        flexibleSpace: ClipRect(
+          child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 0, sigmaY: 2),
+              child: Container(color: Color(0xff2D2C33))),
+        ),
+        title: Text(
+          "我的二维码",
+          style: new TextStyle(fontSize: 22),
+        ),
+        leading: IconButton(
+          iconSize: 25,
+          alignment: Alignment.center,
+          icon: Image.asset(
+            "assets/images/navbar/navbar_back.png",
+            fit: BoxFit.contain,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        actions: [
+          IconButton(
+              constraints: BoxConstraints(maxWidth: 44, maxHeight: 25),
+              alignment: Alignment.center,
+              icon: Image.asset("assets/images/navbar/navbar_more.png"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              }),
+        ],
+      ),
+    );
     return Container(
       alignment: Alignment.center,
       child: Text("User Page!"),
